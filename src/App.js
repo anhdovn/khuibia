d00a7fbcccae91a772ab366d8dc072e665d61dc1
anhdoursa <anh.do@ursa.vn>
2023-06-11 17:16:10 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+import "./App.css";
+import { getProject } from "@theatre/core";
+import studio from "@theatre/studio";
+import extension from "@theatre/r3f/dist/extension";
+import { editable as e, SheetProvider, PerspectiveCamera } from "@theatre/r3f";
+import * as THREE from "three";
+import state from "./state.json";
+import { useEffect } from "react";
+import { ChaiBia } from "./components/ChaiBia";
+import Scene from "./components/Scene";
+
+// studio.initialize();
+// studio.extend(extension);
+
+// const project = getProject("Project");
+// const sheet = project.sheet("Khui bia");
 
 function App() {
+  // useEffect(() => {
+  //   sheet.project.ready.then(() =>
+  //     sheet.sequence.play({ interationCount: Infinity, range: [0, 3] })
+  //   );
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      {/* <SheetProvider> */}
+      {/* <PerspectiveCamera
+          theatreKey="Camera"
+          makeDefault
+          position={[5, 5, -5]}
+          fov={75}
+        /> */}
+      <Scene />
+      <ambientLight />
+      {/* </SheetProvider> */}
+    </Canvas>
   );
 }
 
